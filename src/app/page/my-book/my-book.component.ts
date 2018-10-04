4f851f97bacfcb3967cc31f66976c2d2d6b29b93
Vlad <veliantos@gmail.com>
2018-10-04 22:59:07 +0300

    completed UI aside

--- src/app/page/my-book/my-book.component.ts
+++ src/app/page/my-book/my-book.component.ts
@@ -2,7 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import {
   faArrowCircleDown,
   faCog,
-  faQuestionCircle
+  faQuestionCircle,
+  faPlus,
+  faBook,
+  faGlobeAmericas,
+  faShoppingCart,
+  faStar,
+  faClock,
+  faListAlt,
+  faCircle
 } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -14,8 +22,27 @@ export class MyBookComponent implements OnInit {
   faArrowCircleDown = faArrowCircleDown;
   faCog = faCog;
   faQuestionCircle = faQuestionCircle;
+  faPlus = faPlus;
   users = [
     {img: '../../../assets/img_avatar.png', name: 'John Doe'}
+  ];
+  linksList = [
+    {icon: faBook, name: 'Now Reading', radio: 'radio1'},
+    {icon: faGlobeAmericas, name: 'Browse', radio: 'radio2'},
+    {icon: faShoppingCart, name: 'Buy Books', radio: 'radio3'},
+    {icon: faStar, name: 'Favorite Books', radio: 'radio4'},
+    {icon: faListAlt, name: 'WishList', radio: 'radio5'},
+    {icon: faClock, name: 'History', radio: 'radio7'}
+  ];
+  linksTypes = [
+    {icon: faCircle, name: 'Must Read Titles', radio2: 'radio8', class: 'red'},
+    {icon: faCircle, name: 'Best Of List', radio2: 'radio9', class: 'orange'},
+    {icon: faCircle, name: 'Classic Novels', radio2: 'radio10', class: 'blue'},
+    {icon: faCircle, name: 'Non Fiction', radio2: 'radio11', class: 'violete'},
+  ];
+  alerts = [
+    {icon: faClock, name: 'You added Fight Club by Chuck Palahniuk to your Must Read Title', time: '48 minutes ago'},
+    {icon: faClock, name: 'You added Fight Club by Chuck Palahniuk to your Must Read Title', time: '48 minutes ago'}
   ];
   constructor() { }
 
