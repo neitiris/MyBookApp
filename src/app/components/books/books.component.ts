95bbff89355ce534cff761ddfd5db20c2f9cde53
Vlad <veliantos@gmail.com>
2018-10-06 18:00:01 +0300

    created shared interfaces created shared mock-data

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,15 +1,13 @@
 import {Component, Input, OnInit} from '@angular/core';
+import { IBook } from '../../../shared/interfaces';
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.scss']
 })
 export class BooksComponent implements OnInit {
-  @Input() book_name: string;
-  @Input() book_author: string;
-  @Input() book_image: string;
-  @Input() book_rating: string;
-  @Input() book_id: string;
+  @Input() book: IBook;
   constructor() { }
 
   ngOnInit() {
---
+++ src/shared/interfaces.ts
@@ -0,0 +1,28 @@
+export interface IBook {
+  id?: string|number;
+  name: string;
+  author: string;
+  img: string;
+  rating: string;
+}
+export interface ILinkList {
+  icon: any;
+  name: string;
+  radio: string;
+}
+export interface ILinkTypes {
+  icon: any;
+  name: string;
+  radio2: string;
+  class: string;
+}
+export interface IAlert {
+  icon: any;
+  name: string;
+  time: string;
+}
+export interface IFilter {
+  filter1: string;
+  name: string;
+  title: string;
+}
